ecb15c9b00a2479308a3b3b6e5c3da048a078cae
Manish Gautam <manishgtm123@gmail.com>
2024-03-11 17:19:04 +0545

    Added some Js

--- script.js
+++ script.js
@@ -3,22 +3,29 @@ const toFahrenheit = document.getElementById("toFahrenheit");
 const toCelsius = document.getElementById("toCelsius");
 const result = document.getElementById("result");
 const btn = document.getElementById("btn");
-let temp ;
+let temp;
+
+// inputTask.addEventListener("click", convertTemp);
+textBox.addEventListener("keypress", function(event){
+    if (event.key === "Enter") {
+        btn.click();
+    }
+});
 
 btn.onclick = function () {
     temp = textBox.value;
-    if(temp == null){
+    if (temp == "") {
         result.textContent = `Enter Temperature`;
     }
     else if (toFahrenheit.checked) {
         temp = temp * 9 / 5 + 32;
-        result.textContent = `${temp.toFixed(1)}°F`
+        result.textContent = `${temp.toFixed(1)}°F`;
     }
     else if (toCelsius.checked) {
         temp = (temp - 32) * (5 / 9);
-        result.textContent = `${temp.toFixed(1)}°C`
+        result.textContent = `${temp.toFixed(1)}°C`;
     }
     else {
-        result.textContent = `Select a unit`
+        result.textContent = `Select a unit`;
     }
 }
